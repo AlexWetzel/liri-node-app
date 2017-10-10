b9d633e6f74e8a68bad264db0a6a3cac856543df
AlexWetzel <wetzela@live.com>
2017-10-10 14:25:32 -0400

    Added files and began working on app

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'RBiCgwHUHwIqGgRYyIwr6oP1I',
+  consumer_secret: '0Vjn7QvK8gCanmzI5DK7QxmczWUo3KkJyWH8m6OBdUWToCH4PS',
+  access_token_key: '917800911837294593-OwIJrrE75aQW1epUyGTcSHhCZpB2kZ7',
+  access_token_secret: 'Iz8pI1CCooeT9XKXvia5ft0S9vphVYKrmxYfh9LTblNYv',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,16 @@
+var keys = require("keys.js");
+
+var command = process.argv[2];
+
+if(command === "my-tweets") {
+
+}
+if(command === "spotify-this-song") {
+
+}
+if(command === "movie-this") {
+
+}
+if(command === "do-what-it-says") {
+
+}
